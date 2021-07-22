2880bae5b13bce2d8fac31424d4bd49c0684ca99
Xromenkov <xromenkovevgenii@mail.ru>
2021-07-22 21:33:42 +0300

    Добавлена функция клика по стикерам и вывода блока выбора цвета стикера

--- js/main.js
+++ js/main.js
@@ -1,22 +1,39 @@
 const stick = document.querySelectorAll('.calculator .stickers__item');
 
-// const stickColor = document.querySelectorAll('.calculator .')
+
+
+
+
+// Реализация клика по стикерам в калькуляторе: 
+// - stickRemoveClass - перебирает все стики и ищет тот который содержит класс stick--active, и если он есть то удаляет его;
+// - уже затем добавляет стику, по которому кликнул пользователь класс stick--active и выводит блок с выбором цвета стика;
+// - stickBlock() - всем остальным стикам добавляет класс error, для того что бы реализовать их неактивность
 for (let i = 0; i < stick.length; i++){
     stick[i].onclick = function(){
-        this.classList.toggle('stick--active');
-        // stickBlock();
+        stickRemoveClass();
+        this.classList.add('stick--active');
+        stickBlock();
     }
-
 }
 
-// function stickBlock(){
-//     for (let i = 0; i < stick.length; i++) {
-//         if (stick[i].classList.contains('test')){
-//             // stick[i].classList.add('rename');
-//             console.log(verno)
-//         } 
-//         // else {
-//         //     stick[i].classList.add('error');
-//         // }
-//     }
-// }
+
+function stickRemoveClass () {
+    for(let i = 0; i < stick.length; i++) {
+        if (stick[i].classList.contains('stick--active')){
+            stick[i].classList.remove('stick--active');
+        }
+    }
+}
+
+function stickBlock(){
+    for (let i = 0; i < stick.length; i++) {
+        if (stick[i].classList.contains('stick--active')){
+            stick[i].classList.remove('error');
+        } 
+        else {
+            stick[i].classList.add('error');
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------------
